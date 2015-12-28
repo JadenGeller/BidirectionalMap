38c9a249d6e3bb98aab192546dbcb158f61ca261
Jaden Geller <jaden.geller@gmail.com>
2015-12-27 18:02:00 -0800

    Initial Commit

---
+++ BidirectionalMapTests/BidirectionalMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  BidirectionalMapTests.swift
+//  BidirectionalMapTests
+//
+//  Created by Jaden Geller on 12/27/15.
+//  Copyright © 2015 Jaden Geller. All rights reserved.
+//
+
+import XCTest
+@testable import BidirectionalMap
+
+class BidirectionalMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
